4c685abfd02c3e986bb555f501f85a525d73c4f1
izzy <izzles.martin@gmail.com>
2022-08-10 13:05:53 -0600

    Renamed input files, and added function for part 2.

--- 1.py
+++ 1.py
@@ -1,5 +1,7 @@
 # Day 1: Sonar Sweep
 
+# function for part 1
+#   counts the number of depth increases from one line to the next
 def depth_increase_count(depths):
     increased_count = 0
 
@@ -15,17 +17,42 @@ def depth_increase_count(depths):
     return increased_count
 
 
-with open("./inputs/day1part1.txt", "r+") as file:
+# function for part 2
+#   counts the number of depth increases using a 3 depth sliding window
+def depth_sliding_window_count(depths):
+    increased_count = 0
+
+    depths_split = depths.split()
+
+    for index, line in enumerate(depths_split):
+        if index <= 2:
+            pass
+        else:
+            prev_window = int(depths_split[index - 1]) + int(depths_split[index - 2]) + int(depths_split[index - 3])
+            cur_window = int(depths_split[index]) + int(depths_split[index - 1]) + int(depths_split[index - 2])
+            if cur_window > prev_window:
+                increased_count += 1
+    return increased_count
+
+
+# open test and data file
+with open("inputs/day1test.txt", "r+") as file:
     part1_input = file.read()
 file.close()
 
-part1_count = depth_increase_count(part1_input)
-print(f"Part 1 increases: {part1_count}")
-
-
-with open("./inputs/day1part2.txt", "r+") as file:
+with open("inputs/day1data.txt", "r+") as file:
     part2_input = file.read()
 file.close()
 
-part2_count = depth_increase_count(part2_input)
-print(f"Part 2 increases: {part2_count}")
+# call each function with test and data for part 1 and part 2
+test_count = depth_increase_count(part1_input)
+print(f"Part 1 test input increases: {test_count}")
+
+data_count = depth_increase_count(part2_input)
+print(f"Part 1 data input increases: {data_count}")
+
+test_window = depth_sliding_window_count(part1_input)
+print(f"Part 2 test input sliding window increases: {test_window}")
+
+data_window = depth_sliding_window_count(part2_input)
+print(f"Part 2 data input sliding window increases: {data_window}")
